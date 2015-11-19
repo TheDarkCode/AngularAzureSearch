7ec5e1b4be5449f9a4cfb6f234679ef4c0fe08c8
Mark Hamilton <mark@mark-hamilton.com>
2015-11-19 04:07:49 -0800

    Update Data Indexer
    Moves all required settings to App.config using AppSettingsConfig
    Helper.

---
+++ src/AngularAzureSearch/AngularAzureSearch.DataIndexer/Helpers/AppSettingsConfig.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularAzureSearch.DataIndexer.Helpers
+{
+    class AppSettingsConfig
+    {
+        /// <summary>
+        /// The name of your Azure Search Service.
+        /// </summary>
+        public static string SearchServiceName { get { return ConfigurationManager.AppSettings["searchServiceName"]; } }
+
+        /// <summary>
+        /// The API key used to perform admin functions against your Azure Search Service.
+        /// </summary>
+        public static string SearchServiceApiKey { get { return ConfigurationManager.AppSettings["searchServiceApiKey"]; } }
+
+        /// <summary>
+        /// The full connection string for the DocumentDB database.
+        /// </summary>
+        public static string DbConnectionString { get { return ConfigurationManager.AppSettings["dbConnectionString"]; } }
+
+        /// <summary>
+        /// The DocumentDB collection (name) used as a source for indexing.
+        /// </summary>
+        public static string SourceCollection { get { return ConfigurationManager.AppSettings["sourceCollection"]; } }
+
+        /// <summary>
+        /// The name for the new index you want to create.
+        /// </summary>
+        public static string IndexName { get { return ConfigurationManager.AppSettings["indexName"]; } }
+
+        /// <summary>
+        /// The description for the new index you want to create.
+        /// </summary>
+        public static string IndexDescription { get { return ConfigurationManager.AppSettings["indexDescription"]; } }
+
+        /// <summary>
+        /// The name for the new data source you are creating in Azure Search for your DocumentDB collection.
+        /// </summary>
+        public static string DataSourceName { get { return ConfigurationManager.AppSettings["dataSourceName"]; } }
+        
+        /// <summary>
+        /// The description for the new data source you are creating in Azure Search for your DocumentDB collection.
+        /// </summary>
+        public static string DataSourceDescription { get { return ConfigurationManager.AppSettings["dataSourceDescription"]; } }
+
+        /// <summary>
+        /// The name for the new indexer you want to create.
+        /// </summary>
+        public static string IndexerName { get { return ConfigurationManager.AppSettings["indexerName"]; } }
+
+        /// <summary>
+        /// The description for the new indexer you want to create.
+        /// </summary>
+        public static string IndexerDescription { get { return ConfigurationManager.AppSettings["indexerDescription"]; } }
+
+        /// <summary>
+        /// The name for the new suggester you want to create.
+        /// </summary>
+        public static string SuggesterName { get { return ConfigurationManager.AppSettings["suggesterName"]; } }
+    }
+}
--- src/AngularAzureSearch/AngularAzureSearch.DataIndexer/Program.cs
+++ src/AngularAzureSearch/AngularAzureSearch.DataIndexer/Program.cs
@@ -28,6 +28,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using AngularAzureSearch.DataIndexer.Helpers;
 
 namespace AngularAzureSearch.DataIndexer
 {
@@ -39,31 +40,44 @@ namespace AngularAzureSearch.DataIndexer
         // This Sample shows how to delete, create, upload documents and query an index
         static void Main(string[] args)
         {
-            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
-            string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+            // Key and Name for Destination Azure Search Service
+            string searchServiceName = AppSettingsConfig.SearchServiceName;
+            string apiKey = AppSettingsConfig.SearchServiceApiKey;
+
+            // Settings for DocumentDB Source
+            string dbConnectionString = AppSettingsConfig.DbConnectionString;
+            string sourceCollection = AppSettingsConfig.SourceCollection;
+            string dataSourceName = AppSettingsConfig.DataSourceName;
+            string dataSourceDescription = AppSettingsConfig.DataSourceDescription;
+
+            // Settings for New Index, Suggester, and Indexer
+            string indexName = AppSettingsConfig.IndexName;
+            string indexerName = AppSettingsConfig.IndexerName;
+            string indexerDescription = AppSettingsConfig.IndexerDescription;
+            string suggesterName = AppSettingsConfig.SuggesterName;
 
             // Create an HTTP reference to the catalog index
             _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
-            _indexClient = _searchClient.Indexes.GetClient("trails-index");
+            _indexClient = _searchClient.Indexes.GetClient(indexName);
 
             Console.WriteLine("{0}", "Deleting index...\n");
-            if (DeleteIndex())
+            if (DeleteIndex(indexName))
             {
                 Console.WriteLine("{0}", "Creating index...\n");
-                CreateIndex();
+                CreateIndex(indexName, suggesterName);
                 Console.WriteLine("{0}", "Sync documents from DocumentDB...\n");
-                CreateAndSyncIndexer();
+                CreateAndSyncIndexer(dbConnectionString, dataSourceName, dataSourceDescription, sourceCollection, indexName, indexerName, indexerDescription);
             }
             Console.WriteLine("{0}", "Complete.  Press any key to end application...\n");
             Console.ReadKey();
         }
 
-        private static bool DeleteIndex()
+        private static bool DeleteIndex(string indexName)
         {
             // Delete the index if it exists
             try
             {
-                _searchClient.Indexes.Delete("trails-index");
+                _searchClient.Indexes.Delete(indexName);
             }
             catch (Exception ex)
             {
@@ -75,20 +89,20 @@ namespace AngularAzureSearch.DataIndexer
             return true;
         }
 
-        private static void CreateIndex()
+        private static void CreateIndex(string indexName, string suggesterName)
         {
             // Create the Azure Search index based on the included schema
             try
             {
                 // Create the suggester for suggestions
                 Suggester sg = new Suggester();
-                sg.Name = "trailsg";
+                sg.Name = suggesterName;
                 sg.SearchMode = SuggesterSearchMode.AnalyzingInfixMatching;
                 sg.SourceFields = new List<string>() { "name", "county" };
 
                 var definition = new Index()
                 {
-                    Name = "trails-index",
+                    Name = indexName,
                     Fields = new[]
                     {
                         new Field("id",     DataType.String)         { IsKey = true,  IsSearchable = false, IsFilterable = false, IsSortable = false, IsFacetable = false, IsRetrievable = true},
@@ -109,42 +123,42 @@ namespace AngularAzureSearch.DataIndexer
 
         }
 
-        private static void CreateAndSyncIndexer()
+        private static void CreateAndSyncIndexer(string dbConnectionString, string dataSourceName, string dataSourceDescription, string targetCollection, string indexName, string indexerName, string indexerDescription)
         {
             // Create a new indexer and sync it
             try
             {
-                var creds = new DataSourceCredentials("AccountEndpoint=[YOUR DOCUMENTDB CONNECTION STRING ENDING IN ';Database=YOURDBNAME']");
-                DataSource ds = new DataSource("trails-datasource", DataSourceType.DocumentDb, creds, new DataContainer("Trails"));
-                ds.Description = "Trails Dataset";
+                var creds = new DataSourceCredentials(dbConnectionString);
+                DataSource ds = new DataSource(dataSourceName, DataSourceType.DocumentDb, creds, new DataContainer(targetCollection));
+                ds.Description = dataSourceDescription;
 
                 Indexer idx = new Indexer();
-                idx.Name = "trailsix";
-                idx.Description = "Trails data indexer";
-                idx.DataSourceName = "trails-datasource";
-                idx.TargetIndexName = "trails-index";
+                idx.Name = indexerName;
+                idx.Description = indexerDescription;
+                idx.DataSourceName = dataSourceName;
+                idx.TargetIndexName = indexName;
                 idx.Parameters = new IndexingParameters();
                 idx.Parameters.MaxFailedItems = 10;
                 idx.Parameters.MaxFailedItemsPerBatch = 5;
                 idx.Parameters.Base64EncodeKeys = false;
 
                 //Delete indexer and datasource if it existed
-                _searchClient.DataSources.Delete("trails-datasource");
-                _searchClient.Indexers.Delete("trailsix");
+                _searchClient.DataSources.Delete(dataSourceName);
+                _searchClient.Indexers.Delete(indexerName);
 
                 //Create indexer and datasource
                 _searchClient.DataSources.Create(ds);
                 _searchClient.Indexers.Create(idx);
 
                 //Launch the sync and then monitor progress until complete
-                AzureOperationResponse response = _searchClient.Indexers.Run("trailsix");
+                AzureOperationResponse response = _searchClient.Indexers.Run(indexerName);
                 IndexerGetStatusResponse statusResponse;
                 bool running = true;
 
                 Console.WriteLine("{0}", "Synchronization running...\n");
                 while (running)
                 {
-                    statusResponse = _searchClient.Indexers.GetStatus("trailsix");
+                    statusResponse = _searchClient.Indexers.GetStatus(indexerName);
                     if (statusResponse.StatusCode != HttpStatusCode.OK)
                     {
                         Console.WriteLine("Error polling for indexer status.  Status Code: {0}", response.StatusCode.ToString());
