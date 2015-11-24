aa6a2b948a9dc60c3725208660f1676468de584b
Mark Hamilton <mark@mark-hamilton.com>
2015-11-24 00:50:37 -0800

    Update DocumentClientHelper.cs
    Adds some additional functions and comments.

--- src/AngularAzureSearch/AngularAzureSearch.WebAPI/Helpers/DocumentClientHelper.cs
+++ src/AngularAzureSearch/AngularAzureSearch.WebAPI/Helpers/DocumentClientHelper.cs
@@ -39,6 +39,66 @@ namespace AngularAzureSearch.WebAPI.Helpers
         /// <param name="client">The DocumentDB client instance.</param>
         /// <param name="id">The id of the Database to search for, or create.</param>
         /// <returns>The matched, or created, Database object</returns>
+        private static async Task<Database> GetOrCreateDatabaseAsync(DocumentClient client, string id)
+        {
+            // Get the database by name, or create a new one if one with the name provided doesn't exist.
+            // Create a query object for database, filter by name.
+            IEnumerable<Database> query = from db in client.CreateDatabaseQuery()
+                                          where db.Id == id
+                                          select db;
+
+            // Run the query and get the database (there should be only one) or null if the query didn't return anything.
+            // Note: this will run synchronously. If async exectution is preferred, use IDocumentServiceQuery<T>.ExecuteNextAsync.
+            Database database = query.FirstOrDefault();
+            if (database == null)
+            {
+                // Create the database.
+                database = await client.CreateDatabaseAsync(new Database { Id = id });
+            }
+
+            return database;
+        }
+
+        /// <summary>
+        /// This method uses a ReadDatabaseFeedAsync method to read a list of all databases on the account.
+        /// It demonstrates a pattern for how to control paging and deal with continuations
+        /// This should not be needed for reading a list of databases as there are unlikely to be many hundred,
+        /// but this same pattern is introduced here and can be used on other ReadFeed methods.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <returns>A List of Database entities</returns>
+        private static async Task<List<Database>> ListDatabasesAsync(DocumentClient client)
+        {
+            string continuation = null;
+            List<Database> databases = new List<Database>();
+
+            do
+            {
+                FeedOptions options = new FeedOptions
+                {
+                    RequestContinuation = continuation,
+                    MaxItemCount = 50
+                };
+
+                FeedResponse<Database> response = await client.ReadDatabaseFeedAsync(options);
+                foreach (Database db in response)
+                {
+                    databases.Add(db);
+                }
+
+                continuation = response.ResponseContinuation;
+            }
+            while (!String.IsNullOrEmpty(continuation));
+
+            return databases;
+        }
+
+        /// <summary>
+        /// Get a Database by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="id">The id of the Database to search for, or create.</param>
+        /// <returns>The matched, or created, Database object</returns>
         public static async Task<Database> GetNewDatabaseAsync(DocumentClient client, string id)
         {
             Database database = client.CreateDatabaseQuery().Where(db => db.Id == id).ToArray().FirstOrDefault();
@@ -49,6 +109,17 @@ namespace AngularAzureSearch.WebAPI.Helpers
 
             database = await client.CreateDatabaseAsync(new Database { Id = id });
             return database;
+        }
+
+        /// <summary>
+        /// Deletes a Database resource
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="databaseLink">The SelfLink of the Database resource to be deleted</param>
+        /// <returns></returns>
+        private static async Task DeleteDatabaseAsync(DocumentClient client, string databaseLink)
+        {
+            await client.DeleteDatabaseAsync(databaseLink);
         }
 
         /// <summary>
@@ -97,6 +168,24 @@ namespace AngularAzureSearch.WebAPI.Helpers
         }
 
         /// <summary>
+        /// Get a DocumentCollection by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <param name="dbLink">The Database SelfLink property where this DocumentCollection exists / will be created</param>
+        /// <param name="id">The id of the DocumentCollection to search for, or create.</param>
+        /// <returns>The matched, or created, DocumentCollection object</returns>
+        private static async Task<DocumentCollection> GetOrCreateCollectionAsync(DocumentClient client, string dbLink, string id)
+        {
+            DocumentCollection collection = client.CreateDocumentCollectionQuery(dbLink).Where(c => c.Id == id).ToArray().FirstOrDefault();
+            if (collection == null)
+            {
+                collection = await client.CreateDocumentCollectionAsync(dbLink, new DocumentCollection { Id = id });
+            }
+
+            return collection;
+        }
+
+        /// <summary>
         /// Creates a new collection.
         /// </summary>
         /// <param name="client">The DocumentDB client instance.</param>
@@ -128,6 +217,44 @@ namespace AngularAzureSearch.WebAPI.Helpers
             }
 
             return collection;
+        }
+
+        /// <summary>
+        /// This method uses a ReadCollectionsFeedAsync method to read a list of all collections on a database.
+        /// It demonstrates a pattern for how to control paging and deal with continuations
+        /// This should not be needed for reading a list of databases as there are unlikely to be many hundred,
+        /// but this same pattern is introduced here and can be used on other ReadFeed methods.
+        /// </summary>
+        /// <param name="client">The DocumentDB client instance.</param>
+        /// <returns>A List of Collection entities</returns>
+        private static async Task<List<DocumentCollection>> ReadCollectionsFeedAsync(
+            DocumentClient client,
+            string databaseSelfLink)
+        {
+
+            string continuation = null;
+            List<DocumentCollection> collections = new List<DocumentCollection>();
+
+            do
+            {
+                FeedOptions options = new FeedOptions
+                {
+                    RequestContinuation = continuation,
+                    MaxItemCount = 50
+                };
+
+                FeedResponse<DocumentCollection> response = (FeedResponse<DocumentCollection>)await client.ReadDocumentCollectionFeedAsync(databaseSelfLink, options);
+
+                foreach (DocumentCollection col in response)
+                {
+                    collections.Add(col);
+                }
+
+                continuation = response.ResponseContinuation;
+
+            } while (!String.IsNullOrEmpty(continuation));
+
+            return collections;
         }
 
         /// <summary>
