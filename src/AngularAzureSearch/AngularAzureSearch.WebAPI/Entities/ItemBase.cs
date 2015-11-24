0739dc741a30a5de5cc02305c8784e4a0151acec
Mark Hamilton <mark@mark-hamilton.com>
2015-11-24 00:28:01 -0800

    Add Sample Partition Resolvers and Helpers
    Adds in example code from the azure-documentdb-dotnet github project.
    Credit is given to Ryan CrawCour and Aravind Ramachandran.

--- src/AngularAzureSearch/AngularAzureSearch.WebAPI/Entities/ItemBase.cs
+++ src/AngularAzureSearch/AngularAzureSearch.WebAPI/Entities/ItemBase.cs
@@ -11,22 +11,22 @@ namespace AngularAzureSearch.WebAPI.Entities
     /// <summary>
     /// This is the base class that all root item classes inherit.  This will 
     /// allow the type of item to be passed in which is used for the 
-    /// Where predicate in the RepositoryBase class. This is part of the sample
-    /// modulo hash partition resolver for multi tenant applications.
+    /// Where predicate in the RepositoryBase class.
     /// </summary>
     public class ItemBase
     {
-            private readonly string _docType;
+        private readonly string _docType;
         private readonly string _tenantId;
 
-            /// <summary>
-            /// All root entities inherit this base class.
-            /// </summary>
-            /// <param name="docType">The name of the type of entity (lowercase).</param>
-            public ItemBase(string docType)
-            {
-                this._docType = docType;
-            }
+        /// <summary>
+        /// All root entities inherit this base class.
+        /// </summary>
+        /// <param name="docType">The name of the type of entity (lowercase).</param>
+        public ItemBase(string tenantId, string docType)
+        {
+            this._tenantId = tenantId;
+            this._docType = docType;
+        }
 
         /// <summary>
         /// This is needed for querying in the RepositoryBase. Used by DocumentDB.
@@ -38,14 +38,14 @@ namespace AngularAzureSearch.WebAPI.Entities
         /// This is needed for placing the document in the correct location.
         /// </summary>
         [JsonProperty(PropertyName = "tenantId")]
-            public string tenantId { get; set; }
+        public string tenantId { get { return _tenantId; } }
 
-            /// <summary>
-            /// This docType field will be used to organize the documents by "docType" in 
-            /// DocumentDB in a single-collection scenario.  The docType is just the lowercase
-            /// name of the derived class.
-            /// </summary>
-            [JsonProperty(PropertyName = "docType")]
-            public string docType { get { return _docType; } }
-        }
+        /// <summary>
+        /// This docType field will be used to organize the documents by "docType" in 
+        /// DocumentDB in a single-collection scenario.  The docType is just the lowercase
+        /// name of the derived class.
+        /// </summary>
+        [JsonProperty(PropertyName = "docType")]
+        public string docType { get { return _docType; } }
+    }
 }
